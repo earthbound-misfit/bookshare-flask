d6d5eaa956336e4039cd92cc28ddc426e99ab33a
Kelly Karner <kellymkarner@gmail.com>
2022-11-08 12:46:07 -0600

    debugged errors

--- models.py
+++ models.py
@@ -1,50 +1,52 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-import uuid
+import uuid 
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin, LoginManager
-from flask_marshmallow import Marshmallow
+from flask_login import UserMixin
+from flask_login import LoginManager
+from flask_marshmallow import Marshmallow 
 import secrets
 
 login_manager = LoginManager()
 ma = Marshmallow()
 db = SQLAlchemy()
 
+@login_manager.user_loader
 def load_user(user_id):
-  return User.query.get(user_id)
+    return User.query.get(user_id)
 
 class User(db.Model, UserMixin):
-  id = db.Column(db.String, primary_key=True)
-  first_name = db.Column(db.String(150), nullable=True, default='')
-  last_name = db.Column(db.String(150), nullable=True, default='')
-  email = db.Column(db.String(150), nullable=False)
-  password = db.Column(db.String, nullable=True, default='')
-  g_auth_verify = db.Column(db.Boolean, default=False)
-  token = db.Column(db.String, default='', unique=True)
-  date_created = db.Column(db.Datetime, nullable=False, default=datetime.utcnow)
+    id = db.Column(db.String, primary_key=True)
+    first_name = db.Column(db.String(150), nullable=True, default='')
+    last_name = db.Column(db.String(150), nullable = True, default = '')
+    email = db.Column(db.String(150), nullable = False)
+    password = db.Column(db.String, nullable = True, default = '')
+    g_auth_verify = db.Column(db.Boolean, default = False)
+    token = db.Column(db.String, default = '', unique = True )
+    date_created = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
 
-  def __init__(self, email, first_name='', last_name='', password='', token='', g_auth_verify=False):
-    self.id = self.set_id()
-    self.first_name = first_name
-    self.last_name = last_name
-    self.password = self.set_password(password)
-    self.email = email
-    self.token = self.set_token(24)
-    self.g_auth_verify = g_auth_verify
+    def __init__(self, email, first_name='', last_name='', password='', token='', g_auth_verify=False):
+        self.id = self.set_id()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = self.set_password(password)
+        self.email = email
+        self.token = self.set_token(24)
+        self.g_auth_verify = g_auth_verify
 
-  def set_token(self, length):
-    return secrets.token_hex(length)
-  
-  def set_id(self):
-    return str(uuid.uuid4())
+    def set_token(self, length):
+        return secrets.token_hex(length)
 
-  def set_password(self, password):
-    self.pw_hash = generate_password_hash(password)
-    return self.pw_hash
+    def set_id(self):
+        return str(uuid.uuid4())
+    
+    def set_password(self, password):
+        self.pw_hash = generate_password_hash(password)
+        return self.pw_hash
 
-  def __repr__(self):
-    return f'User with email {self.email} has been added successfully!'
+    def __repr__(self):
+        return f'User {self.email} has been added to the database'
 
 
 class Book(db.Model):
@@ -52,7 +54,7 @@ class Book(db.Model):
   title = db.Column(db.String(200), nullable=False)
   author = db.Column(db.String(200), nullable=True)
   isbn = db.Column(db.String(150), nullable=True)
-  pages = db.Column(db.Integer(5), nullable=True)
+  pages = db.Column(db.Integer(), nullable=True)
   user_token = db.Column(db.String, db.ForeignKey('user.token'), nullable=False)
 
   def __init__(self, title, author, isbn, pages, user_token, id=''):
