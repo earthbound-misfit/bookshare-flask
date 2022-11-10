b62a976ae657de8c74302172137faca5dcc170bc
Kelly Karner <kellymkarner@gmail.com>
2022-11-10 10:40:42 -0600

    edited styles

--- config.py
+++ config.py
@@ -7,6 +7,6 @@ load_dotenv(os.path.join(basedir, '.env'))
 class Config():
   FLASK_APP = os.getenv('FLASK_APP')
   FLASK_ENV = os.getenv('FLASK_ENV')
-  SECRET_KEY = os.environ.get('SECRET_KEY')
+  SECRET_KEY = os.environ.get('SECRET_KEY') or 'This is a secret key'
   SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
   SQLALCHEMY_TRACK_NOTIFICATIONS = False
