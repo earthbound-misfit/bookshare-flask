b9de065240b54517aa1ed224afbf8cfe0fda7b18
Kelly Karner <kellymkarner@gmail.com>
2022-11-10 12:06:21 -0600

    added api post route

--- app/api/routes.py
+++ app/api/routes.py
@@ -3,3 +3,21 @@ from helpers import token_required
 from models import db, User, Book, book_schema, books_schema
 
 api = Blueprint('api', __name__, url_prefix='/api')
+
+@api.route('/bookshelf', methods = ['POST'])
+@token_required
+def add_book(current_user_token):
+  title = request.json['title']
+  author = request.json['author']
+  isbn = request.json['isbn']
+  pages = request.json['pages']
+  user_token = current_user_token.token
+
+  book = Book(title, author, isbn, pages, user_token=user_token)
+
+  db.session.add(book)
+  db.session.commit()
+
+  response = book_schema.dump(book)
+  return jsonify(response)
+
